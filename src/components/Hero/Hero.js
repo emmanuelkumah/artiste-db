821de26579b38fb34461ca75586a2e58c8707290
EmmanuelKumah <mailkumah@gmail.com>
2021-05-03 23:24:49 +0000

    Feature:Add hero section to Hero.js

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,10 +1,32 @@
 import React from "react";
-
+import heroImage from "../../images/dancing.png";
 function Hero() {
   return (
-    <div>
-      <h2>This is the hero component</h2>
-    </div>
+    <section className="hero-section">
+      <div className="hero-container">
+        <div className="row hero-row">
+          <div className="col">
+            <div className="heading-wrapper">
+              <div className="main-heading">
+                <h2>Uncover your favorite Artiste</h2>
+              </div>
+              <div className="sub-heading">
+                <h3>
+                  Find out everthing you need to know about your favorite,
+                  artiste or band
+                </h3>
+              </div>
+              <button className="btn-cta">Click me</button>
+            </div>
+          </div>
+          <div className="col">
+            <div className="image-wrapper">
+              <img src={heroImage} alt="dancing" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
